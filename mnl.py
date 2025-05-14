37d8471b3da0ddccf09ecf383dcee757294ea01f
Jose Fernández Tamame <jftamames@gmail.com>
2025-05-15 00:31:26 +0200

    Update mnl.py

--- mnl.py
+++ mnl.py
@@ -1,11 +1,12 @@
 # mnl.py
 
 import pandas as pd
+import numpy as np
 import statsmodels.api as sm
 
 def fit_mnl(df: pd.DataFrame, features: list[str]):
     """
-    Ajusta un modelo Multinomial Logit (MNLogit) usando Statsmodels.
+    Ajusta un modelo Multinomial Logit usando Statsmodels.
     """
     X = sm.add_constant(df[features])
     y = df["Y"]
@@ -19,7 +20,7 @@ def predict_mnl(model, df: pd.DataFrame, features: list[str]) -> pd.DataFrame:
     """
     X = sm.add_constant(df[features], has_constant="add")
     probs = model.predict(X)  # shape (n_obs, n_cats)
-    # Obtener categorías únicas en el orden que usó el modelo
-    cats = model.model.endog_unique
+    # Obtener categorías únicas de los datos originales
+    cats = np.unique(model.model.endog)
     col_names = [f"P(Y={cat})" for cat in cats]
     return pd.DataFrame(probs, columns=col_names)
